e1f114214d6b4090c4cf43941cb11e37e682b099
Omar Jabour <jabour.omar@hotmail.com>
2025-12-15 17:02:22 +0100

    :art: Set up API & test

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // app.js
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware pour parser le JSON
 app.use(express.json());
 
-const usersRouter = require('./routes/users');
-app.use('/users', usersRouter);
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
 // Route de test
-app.get('/', (req, res) => {
-  res.send('Hello World !');
+app.get("/", (req, res) => {
+  res.send("Hello World !");
 });
 
-// Lancer le serveur
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.get("/status", (req, res) => {
+  res.json({ status: "API is running" });
 });
+
 module.exports = app;
